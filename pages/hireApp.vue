3866c17e012a8baa933d84de90195ffc8f99ad30
Joshua Woods <nordev@protonmail.com>
2023-12-25 22:23:09 -0600

    updaTE

---
+++ pages/hireApp.vue
@@ -0,0 +1,8 @@
+
+<template>
+    <div>
+      <h1>About sPage</h1>
+      <p>This is a simple about page for my Nuxt.js 3 project.</p>
+    </div>
+</template>
+  
---
+++ pages/noteApp.vue
@@ -0,0 +1,8 @@
+
+<template>
+    <div>
+      <h1>About Page</h1>
+      <p>This is a simple about page for my Nuxt.js 3 project.</p>
+    </div>
+</template>
+  
