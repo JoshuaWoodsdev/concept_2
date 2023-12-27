6ff308fbc5edf459b9484db8a5b8d90a2c17c716
Joshua Woods <nordev@protonmail.com>
2023-12-27 12:53:42 -0600

    redirection of app

--- pages/about.vue
+++
@@ -1,9 +0,0 @@
-<!-- pages/about.vue -->
-
-<template>
-    <div>
-      <h1>About Page</h1>
-      <p>This is a simple about page for my Nuxt.js 3 project.</p>
-    </div>
-</template>
-  
--- pages/hireApp.vue
+++
@@ -1,8 +0,0 @@
-
-<template>
-    <div>
-      <h1>About sPage</h1>
-      <p>This is a simple about page for my Nuxt.js 3 project.</p>
-    </div>
-</template>
-  
